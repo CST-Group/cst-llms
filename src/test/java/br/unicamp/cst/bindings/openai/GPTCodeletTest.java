25ff011e2ff179d755e22e6c5972faa5e7c53c97
wandemberg-eld <wandemberg.gibaut@eldorado.org.br>
2024-10-07 17:04:38 -0300

    corrected commit history

--- src/test/java/br/unicamp/cst/bindings/openai/GPTCodeletTest.java
+++ src/test/java/br/unicamp/cst/bindings/openai/GPTCodeletTest.java
@@ -11,19 +11,10 @@
 package br.unicamp.cst.bindings.openai;
 
 import br.unicamp.cst.core.entities.Mind;
-import com.google.gson.JsonPrimitive;
-import io.github.sashirestela.openai.SimpleOpenAI;
-import io.github.sashirestela.openai.domain.chat.Chat;
-import io.github.sashirestela.openai.domain.chat.ChatMessage;
-import io.github.sashirestela.openai.domain.chat.ChatRequest;
-import org.json.JSONObject;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.concurrent.CompletableFuture;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -32,56 +23,65 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 
 public class GPTCodeletTest {
+    final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
 
-
-    GPTCodelet gptCodelet = new GPTCodelet(System.getenv("OPENAI_API_KEY")) {
-
-        /*public HashMap<String, Object> arrangeParams(String userMsg) {
-            HashMap<String, Object> params = new HashMap<String, Object>();
-
-            HashMap<String, Object> fullContent = new HashMap<>();
-            fullContent.put("role", "user");
-            fullContent.put("content", userMsg);
-            JSONObject json = new JSONObject(fullContent);
-
-
-            String message = "[" + json + "]";
-
-            params.put("Authorization", OPENAI_API_KEY);
-            params.put("model", "gpt-4o-mini");
-            params.put("messages", message);
-            params.put("temperature", 0.7);
-
-            return params;
-        } */
-
+    GPTCodelet gptCodelet = new GPTCodelet(OPENAI_API_KEY)
+    {
+        @Override
+        public void accessMemoryObjects() {}
 
         @Override
-        public void accessMemoryObjects() {
-
-        }
-
-        @Override
-        public void calculateActivation() {
-
-        }
+        public void calculateActivation() {}
 
         @Override
         public void proc() {
             String userMsg = "Say this is a test!";
-            /*HashMap<String, Object> params = arrangeParams(userMsg);
-            String answer = "Nothing";
-            try {
-                if(i == 0){
-                answer = sendPOST(chatCompletionEndpoint, params, "application/json");
-                i++;}
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-             */
-
         }
     };
+
+    GPTCodelet gptCodelet_2 = new GPTCodelet()
+    {
+        @Override
+        public void accessMemoryObjects() {}
+
+        @Override
+        public void calculateActivation() {}
+
+        @Override
+        public void proc() {
+            String userMsg = "Say this is a test!";
+        }
+    };
+
+    HashMap<String, Object> config = gptCodelet.configDefaultOpenAI();
+    GPTCodelet gptCodelet_3 = new GPTCodelet(config, OPENAI_API_KEY)
+    {
+        @Override
+        public void accessMemoryObjects() {}
+
+        @Override
+        public void calculateActivation() {}
+
+        @Override
+        public void proc() {
+            String userMsg = "Say this is a test!";
+        }
+    };
+
+    GPTCodelet gptCodelet_4 = new GPTCodelet(config)
+    {
+        @Override
+        public void accessMemoryObjects() {}
+
+        @Override
+        public void calculateActivation() {}
+
+        @Override
+        public void proc() {
+            String userMsg = "Say this is a test!";
+        }
+    };
+
 
     @Test
     public void testResponse() throws InterruptedException {
@@ -93,6 +93,66 @@ public class GPTCodeletTest {
         mind.start();
 
         String answer = gptCodelet.completionOpenAI("Say this is a test!");
+
+        Thread.sleep(2000L);
+
+        mind.shutDown();
+        //assertEquals(expected, answer);
+        System.out.println(answer.toLowerCase());
+        assertTrue(answer.toLowerCase().contains(expected));
+        //System.exit(0);
+    }
+
+    @Test
+    public void testResponse_2() throws InterruptedException {
+        Mind mind = new Mind();
+        String expected = "this is a test!";
+
+        mind.insertCodelet(gptCodelet_2);
+
+        mind.start();
+
+        String answer = gptCodelet_2.completionOpenAI("Say this is a test!");
+
+        Thread.sleep(2000L);
+
+        mind.shutDown();
+        //assertEquals(expected, answer);
+        System.out.println(answer.toLowerCase());
+        assertTrue(answer.toLowerCase().contains(expected));
+        //System.exit(0);
+    }
+
+    @Test
+    public void testResponse_3() throws InterruptedException {
+        Mind mind = new Mind();
+        String expected = "this is a test!";
+
+        mind.insertCodelet(gptCodelet_3);
+
+        mind.start();
+
+        String answer = gptCodelet_3.completionOpenAI("Say this is a test!");
+
+        Thread.sleep(2000L);
+
+        mind.shutDown();
+        //assertEquals(expected, answer);
+        System.out.println(answer.toLowerCase());
+        assertTrue(answer.toLowerCase().contains(expected));
+        //System.exit(0);
+    }
+
+    @Test
+    public void testResponse_4() throws InterruptedException {
+        Mind mind = new Mind();
+        String expected = "this is a test!";
+
+        mind.insertCodelet(gptCodelet_4);
+
+        mind.start();
+
+        String answer = gptCodelet_4.completionOpenAI("Say this is a test!");
 
         Thread.sleep(2000L);
 
